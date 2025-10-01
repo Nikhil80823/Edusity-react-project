60f281f021eed239af5abc3ee9a56c1258ad3e93
Nikhil Bhadwal <nikhilbhadwal808@gmail.com>
2025-10-01 17:23:12 +0530

    made it responsive

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,7 @@ import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
-    <div className='hero container'>
+    <div className='hero container' id="hero">
       <div className="hero-text">
         <h1>We Ensure better education for a better world</h1>
         <p>Our cutting-edge curriculam is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education</p>
