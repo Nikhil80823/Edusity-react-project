ae6387b92cbe20c0fa906745c85b4bdc3ec68c0d
Nikhil Bhadwal <nikhilbhadwal808@gmail.com>
2025-10-01 16:03:08 +0530

    pushing to git

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Hero from './components/hero/hero'
+import Programs from './components/programs/programs'
+import Title from './components/Title/Title'
+import About from './components/About/About'
+import Campus from './components/Campus/Campus'
+import Testimonials from './components/Testimonials/Testimonials'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero/>
+      <div className="container">
+        <Title subTitle='Our PROGRAM'  title= 'What We Offer'/>
+      <Programs/>
+      <About/>
+      <Title subTitle='Gallery'  title= 'Campus Photos'/>
+      <Campus/>
+      <Title subTitle='TESTIMONIALS'  title= 'What Student Says'/>
+      <Testimonials/>
+      <Title subTitle='Contact Us'  title= 'Get In Touch'/>
+      <Contact/>
+      <Footer/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
